df87d8d7651cdbd26a58ee880468df4fe363d65a
bytethenoodle <elbertjohnalejoorozco@gmail.com>
2017-07-23 17:47:36 +0800

    fixed unaligned sources

--- Package.swift
+++ Package.swift
@@ -3,11 +3,11 @@ import PackageDescription
 let package = Package(
     name: "Prorsum",
     targets: [
-        Target(name: "Prorsum", dependencies: ["CHTTPParser"]),
+        Target(name: "Prorsum"),
         Target(name: "Performance", dependencies: ["Prorsum"]),
-        Target(name: "CHTTPParser", dependencies: [])
     ],
     dependencies: [
+        .Package(url: "https://github.com/IBM-Swift/CHTTPParser.git", majorVersion: 0, minor: 5),
         .Package(url: "https://github.com/vapor/clibressl.git", majorVersion: 1)
     ]
 )
